d108c04fd148ec8f1c03ea8cf75c861ffab841cd
H.Nathan <nathan.hervouet@ynov.com>
2017-06-02 15:36:54 +0200

    Ajout Filtrage Maintenance.php

--- maintenance.php
+++ maintenance.php
@@ -9,15 +9,15 @@
 
 <body>
     <div class="container">
-        <input type="text" id="myInput" onkeyup="myFunction()" placeholder="Search for names.." title="Type in a name">
+        <input type="text" id="myInput" onkeyup="myFunction()" placeholder="Rechercher une société" title="Type in a name">
         <div id="table" class="table-editable">
             <table class="table">
                 <tr>
-                    <th>Société</th>
-                    <th>Année</th>
-                    <th>Date</th>
+                    <th onclick="sortTable(0)">Société</th>
+                    <th onclick="sortTable(1)">Année</th>
+                    <th onclick="sortTable(2)">Date</th>
                     <th>Demande</th>
-                    <th>Nombre d'heure</th>
+                    <th onclick="sortTable(3)">Nombre d'heure</th>
                 </tr>
                 <tr>
                     <td contenteditable="true">Aro Conseil</td>
@@ -61,7 +61,6 @@
             }
         });
     </script>
-    
     <script>
         function myFunction() {
             var input, filter, table, tr, td, i;
@@ -77,6 +76,64 @@
                     }
                     else {
                         tr[i].style.display = "none";
+                    }
+                }
+            }
+        }
+    </script>
+    <script>
+        function sortTable(n) {
+            var table, rows, switching, i, x, y, shouldSwitch, dir, switchcount = 0;
+            table = document.getElementById("table");
+            switching = true;
+            //Set the sorting direction to ascending:
+            dir = "asc";
+            /*Make a loop that will continue until
+            no switching has been done:*/
+            while (switching) {
+                //start by saying: no switching is done:
+                switching = false;
+                rows = table.getElementsByTagName("TR");
+                /*Loop through all table rows (except the
+                first, which contains table headers):*/
+                for (i = 1; i < (rows.length - 1); i++) {
+                    //start by saying there should be no switching:
+                    shouldSwitch = false;
+                    /*Get the two elements you want to compare,
+                    one from current row and one from the next:*/
+                    x = rows[i].getElementsByTagName("TD")[n];
+                    y = rows[i + 1].getElementsByTagName("TD")[n];
+                    /*check if the two rows should switch place,
+                    based on the direction, asc or desc:*/
+                    if (dir == "asc") {
+                        if (x.innerHTML.toLowerCase() > y.innerHTML.toLowerCase()) {
+                            //if so, mark as a switch and break the loop:
+                            shouldSwitch = true;
+                            break;
+                        }
+                    }
+                    else if (dir == "desc") {
+                        if (x.innerHTML.toLowerCase() < y.innerHTML.toLowerCase()) {
+                            //if so, mark as a switch and break the loop:
+                            shouldSwitch = true;
+                            break;
+                        }
+                    }
+                }
+                if (shouldSwitch) {
+                    /*If a switch has been marked, make the switch
+                    and mark that a switch has been done:*/
+                    rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
+                    switching = true;
+                    //Each time a switch is done, increase this count by 1:
+                    switchcount++;
+                }
+                else {
+                    /*If no switching has been done AND the direction is "asc",
+                    set the direction to "desc" and run the while loop again.*/
+                    if (switchcount == 0 && dir == "asc") {
+                        dir = "desc";
+                        switching = true;
                     }
                 }
             }
